ba3ffe4c90be2c27095beca6550d7efcf9e97aed
Vijay Tripathi <email@vijaytripathi.com>
2021-10-03 21:08:43 -0400

    final 7z

--- Lab2/2021F_lab_hex/simtop.sv
+++ Lab2/2021F_lab_hex/simtop.sv
@@ -8,7 +8,7 @@ module simtop;
 	logic clk; //defaulted to 1 bit 
 	logic [6:0] HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
 	logic [17:0] SW;
-	logic counter [15:0]; //logic data type, 16 bit wide
+	reg counter [15:0]; //logic data type, 16 bit wide
 	top dut
 	(
 		//////////// CLOCK //////////
@@ -54,20 +54,16 @@ module simtop;
 		clk = 1; #10; clk = 0; #10; //delay for 10 time units; defines minimum granularity of sim
 	end
 
-	always  @(posedge clk) begin  //on positive clock edge, do this
-		counter <= counter + 16'b1; //16 bit 
-		$display("counter=%x\n", counter); //printf
-	end
-
 	always @(negedge clk) begin
 		for (int i = 0; i < 16; i++) begin
+			$display("counter=%x\n", i);
 			if ((i !== 0) && (i !== 15)) begin  // HEX0 only on for counter = 0 and counter = 15
 				if(HEX0 != 7'b1111111) begin
-					$error("HEX0 should have all segs turned off for values 1-14");
+					$error("Value: %d HEX0 should have all segs turned off for values 1-14", i);
 				end
 			end else if (i == 15) begin //switches 0-3 turned on; HEX0 should display a 1
 				if(HEX0 != 7'b1111001) begin
-					$error("HEX0 should display a 1 (7'b1111001)");
+					$error("HEX0 should display a 1 (7'b1111001) for test case %d", i);
 				end
 			end
 		end
