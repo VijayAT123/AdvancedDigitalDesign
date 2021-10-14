6c11042f724d6105f432d14d2b1d4f7549ef5a88
Vijay Tripathi <email@vijaytripathi.com>
2021-10-14 15:07:01 -0400

    control unit

--- Lab2/2021F_lab_hex/hexdriver.sv
+++ Lab2/2021F_lab_hex/hexdriver.sv
@@ -1,17 +1,17 @@
 module hexdriver (input [3:0] val, output logic [6:0] HEX);
 
-	/* your code here */
-
+	// input [3:0] 4 bits; val comparison should match bit width (always specify to debug errors)
 	//could also use assign instead of always begin (followed by nester ternaries)
 	always_comb begin //begin called sensitivity from always sensitivity list
-		if (val == 4'h0) begin //if all 4 switches off 
-// input [3:0] 4 bits; val comparison should match bit width (always specify to debug errors)
-			HEX = 7'b1000000; //all segments except segment 6, diplays a 0
-		end else if (val == 4'hf) begin //if all switches on; 15 in decimal 1111 in binary
+		if (val == 4'b0) //if all 4 switches off 
+			HEX = 7'0b100000; //all segments except segment 6, diplays a 0
+		else if (val == 4'b1) //if first switch on
 			HEX = 7'b1111001; // all segments turned off except 1 and 2 (HEXs in active low); displays a 1
-		end else begin
-			HEX = 7'b1111111; //all segments turned off
-		end
+		else if (val == 4'b10) //if second switch on
+			HEX = 7'b0100100;
+		else if (val == 4'b11) //if first 2 switches on; 3
+			HEX = 7'b0110000;
+		else if (val == 4'b100) // 3rd switch on; 4
 		
 	end
 
--- Lab3/instruction_deocde.sv
+++ Lab3/instruction_deocde.sv
@@ -1,4 +1,17 @@
-module instruction_decode (input [31:0] inst, output inst_type) {
-
+module instruction_decode (
+    input [31:0] inst, 
     
+    //still output funct7, imm, etc. and later choose to use field based on instruction type
+    output [4:0] rs1,
+    output [4:0] rs2,
+    output [4:0] rd,
+    output [2:0] funct3,
+
+    output inst_type) {
+
+    assign opcode = inst [6:0];
+
+    always_comb begin
+        if (opcode == 0b0110011) 
+            inst_type = "R";
 }
