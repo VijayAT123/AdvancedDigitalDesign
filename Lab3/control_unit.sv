980db7228f15edfb5100c3015a312f75e1128f0e
Vijay Tripathi <email@vijaytripathi.com>
2021-10-21 13:04:23 -0400

    almost all r-type done

--- Lab3/control_unit.sv
+++ Lab3/control_unit.sv
@@ -33,17 +33,51 @@ instruction_decode decoder (
 )
 
 always_comb begin 
-    assign regwrite = 1'b1; //except for csrrw HEX
-    assign gpio_we = 1'b0; //except for csrrw HEX
+    regwrite = 1'b1;     //except for csrrw HEX
+    gpio_we = 1'b0;      //except for csrrw HEX
+    regsel = 2'b10;      //exceot fir csrrw and lui
+    regwrite = 1'b1;     //except for csrrw HEX
+    gpio_we = 1'b0;      //except for csrrw HEX
 
-    //add
-    if ((inst_type == 2'b00) && (funct7 == 7'h0) && (funct3 == 3'b0) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)) && (opcode == 7'h33)) begin
-        assign aluop = 4'b0011;
-        assign alusrc = 1'b0;
-        assign regsel = 2'b10;
-
-    //sub 
-    else if ((inst_type == 2'b00) && (funct7 == 7'h20) && (funct3 == 3'b0) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)) && (opcode == 7'h33)) begin
-        assign aluop = 4'b0011;
-        assign alusrc = 1'b0;
-        assign regsel = 2'b10;
+    if(opcode == 7'h33 && inst_type == 2'b00) begin //R-Type insts
+        alusrc = 1'b0;
+        //add
+        if ((funct7 == 7'h0) && (funct3 == 3'b0) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0011;
+        //sub 
+        else if ((funct7 == 7'h20) && (funct3 == 3'b000) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0100;
+        //mul
+        else if ((funct7 == 7'h1) && (funct3 == 3'b000) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0101;
+        //mulh
+        else if ((funct7 == 7'h1) && (funct3 == 3'b001) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0110;
+        //mulhu
+        else if ((funct7 == 7'h1) && (funct3 == 3'b011) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0111;
+        //slt
+        else if ((funct7 == 7'h0) && (funct3 == 3'b010) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b1100;
+        //sltu
+        else if ((funct7 == 7'h0) && (funct3 == 3'b011) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = ???????; 
+            //TODO correct aluop
+        //and
+        else if ((funct7 == 7'h0) && (funct3 == 3'b111) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0000;
+        //or
+        else if ((funct7 == 7'h0) && (funct3 == 3'b110) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0001;
+        //xor
+        else if ((funct7 == 7'h0) && (funct3 == 3'b100) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0010;
+        //sll
+        else if ((funct7 == 7'h0) && (funct3 == 3'b010) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b1000;
+    end
+    else if (opcode == 7'h13 && inst_type == 01 ) begin
+        //add
+        if ((funct7 == 7'h0) && (funct3 == 3'b0) && ((immI === 12'bX)|| (immS === 12'bX)|| (immU=== 12'bX)))
+            aluop = 4'b0011;
+    end
--- Lab3/cpu.sv
+++ Lab3/cpu.sv
@@ -1,17 +1,26 @@
-module cpu ();
+module cpu (
+    output
+);
 
-logic   clk;
-reg     [31:0]  prog_counter;
+logic   [0:0 ]  clk;
+reg     [11:0]  prog_counter; //12 bit wide to match #rows in instruction_mem
+logic   [31:0]  instruction_mem [4095:0];
+
 
 instruction_decode decoder ()
 
 control_unit cu (
     .rs1(decoder.rs1)
 )
+
+initial 
+    $readmemh("hexcode.txt", instruction_mem);
+
 always @(posedge clk, posedge rst) begin
     if (rst)
         prog_counter <= 32'b0;
     else
-        prog_counter <= prog_counter + 1'b1;
+        instruction_EX <= instruction_mem[prog_counter];
+        prog_counter <= prog_counter + 12'b1;
 
 endmodule
