db2f019a30dd750c34a5048e49ce7c4495728dc4
Vijay Tripathi <email@vijaytripathi.com>
2021-10-27 23:22:21 -0400

    "final" cpu design

--- Lab3/cpu.sv
+++ Lab3/cpu.sv
@@ -1,14 +1,7 @@
 module cpu ();
-//todo regsel_EX MUX -> output into regsel_WB
-//todo GPIO register
-//todo imm20 register
-//todo r_ex register -> output into r_wb
-//todo rd register -> output into regdest_WB
-
-
 
 ///////DECODER//////
-logic   [6: 0]  funct7_EX
+logic   [6: 0]  funct7_EX;
 logic   [4: 0]  rs1_EX;
 logic   [4: 0]  rs2_EX;
 logic   [4: 0]  rd_EX;
@@ -16,7 +9,7 @@ logic   [2: 0]  funct3_EX;
 logic   [11:0]  imm12_EX;
 assign  imm12_EX = {20{inst[31], imm12_EX}; //sign extension; add 20 leading bits (bit 31 is sign bit)
 logic   [19:0]  imm20_EX;
-assign  imm20_EX = {imm20_EX, 12'b0};
+assign  imm20_EX = {imm20_EX, 12'b0}; //cat 12 0s to make 32 bits in length
 logic   [6: 0]  opcode_EX;
 
 ///////CONTROL UNIT//////
@@ -27,7 +20,7 @@ logic   [1: 0]   regsel_WB;
 logic   [0: 0]   regwrite_EX;
 logic   [0: 0]   we;
 logic   [0: 0]   gpio_we; 
-logic   [32:0]   gpio_out;
+logic   [31:0]   gpio_out;
 
 ///////ALU///////
 logic   [31:0]   a_EX;
@@ -42,10 +35,12 @@ logic   [31:0]   readdata2_EX;
 ////////CPU////////
 logic   [0: 0]  clk;
 logic   [0: 0]  reset;
-reg     [11:0]  prog_counter_fetch; //12 bit wide to match #rows in instruction_mem
+reg     [11:0]  prog_counter_F; //12 bit wide to match #rows in instruction_mem
 logic   [31:0]  instruction_mem [4095:0]; //4096 x 32
 logic   [2: 0]  inst_type;
 logic   [31:0]  instruction_EX;
+logic   [3: 0]  regdest_WB;
+logic   [31:0]  writedata_WB;
 
 
 
@@ -88,19 +83,18 @@ regfile register (
 	.we         (we),           /* write enable */
 	.readaddr1  (rs1_EX),       /* read address 1 */
 	.readaddr2  (rs2_EX),		/* read address 2 */
-	.writeaddr  (rd_EX),		/* write address */
-	.writedata  (),
+	.writeaddr  (regdest_WB),	/* write address */
+	.writedata  (writedata_WB),
     .readdata1  (readdata1_EX),
     .readdata2  (readdata2_EX)
-)
+);
 
 alu alu (
     .A          (a_EX),
 	.B          (b_EX),
-	.op         (opcode_EX),
-	.R          (r_EX),
-	output logic zero
-)
+	.op         (aluop),
+	.R          (r_EX)
+);
 
 initial 
     $readmemh("hexcode.txt", instruction_mem); //readmemh always in initial
@@ -118,30 +112,42 @@ always_ff @(posedge clk) begin
     regsel_WB <= regsel_EX;
 
     if (regsel_WB == 2'b00)
-        writedata <= //todo GPIO_in register
+        writedata_WB <= gpioin;
     else if(regsel_WB == 2'b01)
-        writedata <= 
+        writedata_WB <= imm20_EX;
+    else if(regsel_WB == 2'b10)
+        writedata_WB <= r_wb;
 end
 
 //rs2 MUX
-always_comb begin 
-    b = alusrc_EX?imm12_EX:readdata2_EX;
-end 
+always_comb 
+    b_EX = alusrc_EX?imm12_EX:readdata2_EX; 
+
+//GPIO_out MUX
+always_comb
+    gpio_out = gpio_we?//todo what is output when enable is off?
 
 //GPIO_in register
 always_ff @ (posedge clk)
-    gpioin
-
-//GPIO MUX
-always_comb begin
-    gpio_out = gpio_we?//todo what is output when enable is off?
-end
+    gpioin <= gpioin;
 
 //regwrite register
 always_ff @ (posedge clk)
     we <= regwrite_EX;
-endmodule
 
 //imm20 register
-always_ff @ (posedge clk) begin
-    
+always_ff @ (posedge clk)
+    imm20_EX <= imm20_EX;
+
+//r_ex -> r_wb register
+always_ff @ (posedge clk)
+    r_WB <= r_EX;
+
+//regdest_wb register
+always_ff @ (posedge clk)
+    rd_EX <= regdest_WB;
+
+always_ff @ (posedge clk)
+    regsel_WB <= regsel_EX;
+
+endmodule
